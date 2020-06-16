73ce4a1243a7c19af647b850818ec2b86df44967
Wanfeng Hu <535561351@qq.com>
2020-06-17 00:35:32 +0800

    add chapter 1

---
+++ Chapter01/Hello.java
@@ -0,0 +1,12 @@
+package Chapter01;
+
+/**
+ * Hello
+ */
+public class Hello {
+
+    public static void main(final String[] args) {
+        final String x = "Test";
+        System.out.println(x);
+    }
+}
