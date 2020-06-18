85780d2ec4424bef2a05bb3a14c85c02b8e5f763
Wanfeng Hu <535561351@qq.com>
2020-06-18 15:03:22 +0800

    add Chapter 6

---
+++ Chapter06/DotCom.java
@@ -0,0 +1,33 @@
+package Chapter06;
+
+import java.util.ArrayList;
+
+public class DotCom {
+    private ArrayList<String> locationCells;
+    private String name;
+
+    public void setName(String s) {
+        name = s;
+    }
+
+    public String checkYourself(String userInput) {
+        String result = "miss";
+        int index = locationCells.indexOf(userInput);
+
+        if (index>=0) {
+            locationCells.remove(index);
+            if (locationCells.isEmpty()) {
+                result = "kill";
+                System.out.println("Ouch! You sunk " + name + "  : (");
+            } else {
+                result = "hit";
+            }
+        }
+        
+        return result;
+    }
+
+    public void setLocationCells(ArrayList<String> locs) {
+        locationCells = locs;
+    }
+}
---
+++ Chapter06/note.md
@@ -0,0 +1,10 @@
+# Notes for Chapter 6
+
+* An ArrayList holds objects and not primitives, the compiler will automatically "wrap" a primitive into a object, and place that object in the ArrayList. ("unwrap" when taking out)
+
+* Import full name of a class except those in `java.lang`.
+
+* Non Short Circuit Operators (`&`, `|`):  
+When used in boolean expressions, they act like `&&` and `||`, but they force the JVM to always check both side of the expression.
+
+* In Java, `import` doesn't make the class bigger. 
